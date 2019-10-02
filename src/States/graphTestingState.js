7e93e70b031ce9c1d2fe1fa3ce79efe88799db8e
Aitor Iribar <aitor.iribar@gmail.com>
2019-10-02 09:28:47 +0200

    Movido código de creación de caminos a una clase reutilizable, y hecho compatible con graphTestingState

--- src/States/graphTestingState.js
+++ src/States/graphTestingState.js
@@ -2,7 +2,11 @@ const DEBUG_GRAPH = false;
 
 "use strict";
 var graphTestingState = function (game) {
-
+    this.CONVEYOR_BELT_NUMBER = 2;
+    this.CONVEYOR_BELT_SPAWN_X = 100;
+    this.CONVEYOR_BELT_SPAWN_Y = 100;
+    this.CONVEYOR_BELT_HORIZONTAL_OFFSET = 100;
+    this.CONVEYOR_BELT_VERTICAL_DISTANCE = 500;
 }
 
 graphTestingState.prototype = {
@@ -14,12 +18,24 @@ graphTestingState.prototype = {
     create : function() {
         console.log("Entered grapthTestingState")
         
-        this.graph = new Graph(CONVEYOR_BELT_NUMBER, CONVEYOR_BELT_SPAWN_X, CONVEYOR_BELT_SPAWN_Y, CONVEYOR_BELT_HORIZONTAL_OFFSET, CONVEYOR_BELT_VERTICAL_DISTANCE);
+        this.graph = new Graph(this.CONVEYOR_BELT_NUMBER, this.CONVEYOR_BELT_SPAWN_X, this.CONVEYOR_BELT_SPAWN_Y, this.CONVEYOR_BELT_HORIZONTAL_OFFSET, this.CONVEYOR_BELT_VERTICAL_DISTANCE);
         if (DEBUG_GRAPH) {
             console.log("Original state of the graph");
             this.graph.printGraph();
         }
-        
+
+        //Crea levelData falso para poder crear un PathCreator
+        let levelData = {
+            lanes: {
+                count: this.CONVEYOR_BELT_NUMBER,
+                startX: this.CONVEYOR_BELT_SPAWN_X,
+                startY: this.CONVEYOR_BELT_SPAWN_Y,
+                gap: this.CONVEYOR_BELT_HORIZONTAL_OFFSET,
+                height: this.CONVEYOR_BELT_VERTICAL_DISTANCE
+            }
+        }
+        this.pathCreator = new PathCreator(this.graph, levelData, this.graph.getColumns());
+
         let pathsToCreate = 20;
         for (let i = 0; i < pathsToCreate; ++i) {
             this.createRandomPath();
@@ -34,14 +50,14 @@ graphTestingState.prototype = {
     },
 
     createRandomPath : function() {
-        let conveyorBeltOriginId = Math.floor(Math.random() * CONVEYOR_BELT_NUMBER);
-        let x0 = CONVEYOR_BELT_SPAWN_X + conveyorBeltOriginId * CONVEYOR_BELT_HORIZONTAL_OFFSET;
-        let y0 = Math.floor(Math.random() * (CONVEYOR_BELT_VERTICAL_DISTANCE - CONVEYOR_BELT_SPAWN_Y) + CONVEYOR_BELT_SPAWN_Y);
+        let conveyorBeltOriginId = Math.floor(Math.random() * this.CONVEYOR_BELT_NUMBER);
+        let x0 = this.CONVEYOR_BELT_SPAWN_X + conveyorBeltOriginId * this.CONVEYOR_BELT_HORIZONTAL_OFFSET;
+        let y0 = Math.floor(Math.random() * (this.CONVEYOR_BELT_VERTICAL_DISTANCE - this.CONVEYOR_BELT_SPAWN_Y) + this.CONVEYOR_BELT_SPAWN_Y);
         let origin = new Vector2D(x0, y0);
 
         let conveyorBeltDestinyIdId = (conveyorBeltOriginId == 1) ? 0 : 1;
-        let xF = CONVEYOR_BELT_SPAWN_X + conveyorBeltDestinyIdId * CONVEYOR_BELT_HORIZONTAL_OFFSET;
-        let yF = Math.floor(Math.random() * (CONVEYOR_BELT_VERTICAL_DISTANCE - y0) + y0);
+        let xF = this.CONVEYOR_BELT_SPAWN_X + conveyorBeltDestinyIdId * this.CONVEYOR_BELT_HORIZONTAL_OFFSET;
+        let yF = Math.floor(Math.random() * (this.CONVEYOR_BELT_VERTICAL_DISTANCE - y0) + y0);
         let destiny = new Vector2D(xF, yF);
 
         if (DEBUG_GRAPH) {
@@ -55,12 +71,14 @@ graphTestingState.prototype = {
         if (!this.bag.reachedTheEnd) {
             this.bag.moveBag();
         }
+
+        this.pathCreator.update();
     },
 
     createRandomBag : function() {
-        let conveyorBeltOriginId = Math.floor(Math.random() * CONVEYOR_BELT_NUMBER);
-        let x = CONVEYOR_BELT_SPAWN_X + conveyorBeltOriginId * CONVEYOR_BELT_HORIZONTAL_OFFSET;
-        let y = CONVEYOR_BELT_SPAWN_Y;
+        let conveyorBeltOriginId = Math.floor(Math.random() * this.CONVEYOR_BELT_NUMBER);
+        let x = this.CONVEYOR_BELT_SPAWN_X + conveyorBeltOriginId * this.CONVEYOR_BELT_HORIZONTAL_OFFSET;
+        let y = this.CONVEYOR_BELT_SPAWN_Y;
         let position = new Vector2D(x, y);
         this.bag = new Bag(0, 0, position, this.graph);
 
--- src/States/waveManagerDevState.js
+++ src/States/waveManagerDevState.js
@@ -11,7 +11,7 @@ levelLoadState se encargará de empezar el estado de gaemplay cuando todo esté 
 */
 waveManagerDevState.prototype = {
 
-    //////////////////
+
     //INITIALIZACIÓN//
     //////////////////
     init: function(levelData) {
@@ -21,102 +21,30 @@ waveManagerDevState.prototype = {
     create: function() {
         console.log("Entered waveManagerDevSate")
         
-        //Set up input events
-        game.input.onDown.add(this.onTouchStart, this);
-        game.input.onUp.add(this.onTouchEnd, this);
-
-        this.createLanes(this.levelData.lanes);
-
+        //Create managers and such
+        this.createGraph(this.levelData.lanes);
+        this.pathCreator = new PathCreator(this.graph, this.levelData, this.lanes);
         this.waveManager = new WaveManager(this.levelData, this.endGame);
+
         this.waveManager.startNextWave();
     },
 
-    createLanes: function(laneInfo) {
+    createGraph: function(laneInfo) {
         this.graph = new Graph(laneInfo.count, laneInfo.startX, laneInfo.startY, laneInfo.gap, laneInfo.height);
         this.lanes = this.graph.getColumns();
     },
 
-    /////////////
     //GAME LOOP//
     /////////////
     update: function() {
         //Visualización provisional
         this.graph.displayGraph();
-        if (this.pathDrawProcess != null) this.displayCurrentDrawnPath(this.pathDrawProcess);
-
-
-
+        
+        this.pathCreator.update();
         this.waveManager.update(game.time.physicsElapsed);        
     },
 
-    displayCurrentDrawnPath: function(pathDrawProcess) {
-        let currentTouchedPoint = new Vector2D(game.input.x, game.input.y);
-        let graphPoint = this.getGraphPointFromTouch(currentTouchedPoint);
-
-        let wrongPathColor = "rgb(128, 0, 0)";
-        let correctPathColor = "rgb(0, 128, 0)";
-        let isValidPath = graphPoint != null && this.graph.pathIsValid(pathDrawProcess.startPoint, graphPoint);
-
-
-        if (isValidPath) {
-            this.graph.displaySection(pathDrawProcess.startPoint, graphPoint, correctPathColor);
-        } else {    
-            this.graph.displaySection(pathDrawProcess.startPoint, currentTouchedPoint, wrongPathColor);
-        }
-    },
-
-    ////////////////////
-    //EVENTOS DE INPUT//
-    ////////////////////
-    onTouchStart: function(pointer) {
-        let point = this.getGraphPointFromTouch(new Vector2D(pointer.x, pointer.y));
     
-        if (point != null)
-        {
-            this.pathDrawProcess = {
-                startPoint: point
-            };
-        }
-    },
-
-    onTouchEnd: function(pointer) {
-        if (this.pathDrawProcess != null) {
-            let endPoint = this.getGraphPointFromTouch(new Vector2D(pointer.x, pointer.y));
-            if (endPoint != null) {
-                this.graph.addPath(this.pathDrawProcess.startPoint, endPoint);
-            }
-
-            this.pathDrawProcess = null;
-        }
-    },
-    
-
-
-    //Devuelve null si el punto no es válido para dibujar un camino, el punto tocado si no
-    getGraphPointFromTouch: function(touchPoint) {
-        //See if the height is fine
-        let minAllowedY = this.levelData.lanes.startY;
-        let maxAllowedY = minAllowedY + this.levelData.lanes.height;
-
-        if (touchPoint.y < minAllowedY || touchPoint.y > maxAllowedY) return null;
-        
-        //See which, if any, lane this is.
-        let laneIndex = -1;
-        let smallestDistance = 100000;
-        for(let i = 0; i < this.lanes.length; i++)
-        {
-            let distance = Math.abs(this.lanes[i] - touchPoint.x);
-            if (distance <= PATH_DRAW_TOLERANCE && distance < smallestDistance) {
-                smallestDistance = distance;
-                laneIndex = i;
-            }
-        }
-
-        if (laneIndex != -1) {
-            return new Vector2D(this.lanes[laneIndex], touchPoint.y);
-        }
-        else return null;
-    },
 
     endGame: function() {
         console.log("Game ended!");
