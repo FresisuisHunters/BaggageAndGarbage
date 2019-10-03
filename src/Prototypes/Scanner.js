f29708a082c5afbb5dffec1fced4807241b8ddf8
César Romero Albertos <43405217+CesarRomeroAlbertos@users.noreply.github.com>
2019-10-03 14:32:24 +0200

    Primer esquema de prototipo de Scanner
    He borrado un montón de cambios porque no iban a funcionar con el sistema nuevo para las bolsas y prefiero hacerlo direcamente adaptándolo a eso que va a ir mejor y ser más sencillo

--- src/Prototypes/Scanner.js
+++ src/Prototypes/Scanner.js
@@ -1,11 +1,15 @@
-Scanner.prototype=
-{
-    Scanner:function(id)
-    {
-        this.id = id;
-        //sprite
-    },
-    EnterBag:function(Bag)
+function Scanner(position) {
+    this.id = id;
+    this.length = 10;
+    this.belt = position.x;
+    //posiciones inicial y final del scanner
+    this.start = position.y;
+    this.end = position + length;
+    //sprite
+}
+
+Scanner.prototype={
+    EnterBag: function(Bag)
     {
         if(this.currentBag == null)
         {
@@ -18,15 +22,16 @@ Scanner.prototype=
             return false;
         }
     },
-    ExitBag()
+    ExitBag: function()
     {
 
         let exitBag = this.currentBag;
+        exitBag.moveBag(this.end);
         this.currentBag = null;
         this.entryTime = null;
         return exitBag;
     },
-    DisplayScanner()
+    DisplayScanner: function()
     {
         
     }
