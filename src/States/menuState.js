afa93adee20c6882333841f763d36b10aef02f1b
Jormii <lopeznatalj@gmail.com>
2019-10-15 17:08:34 +0200

    Error resuelto. Muestra simplemente los sprites

--- src/States/menuState.js
+++ src/States/menuState.js
@@ -13,9 +13,9 @@ const LEVELS_JSON_DIR = "resources/levels/";
 const LEVEL_JSON_PREFIX = "devLevel_";
 const LEVEL_JSON_SUFFIX = ".json";
 
-const LEVEL_CARDS_X = 50;
-const FIRST_LEVEL_Y = 50;
-const LEVEL_CARDS_Y_OFFSET = 50;
+const LEVEL_CARDS_X = 1080 / 2;
+const FIRST_LEVEL_Y = 450;
+const LEVEL_CARDS_Y_OFFSET = 300;
 
 menuState.prototype = {
 
@@ -26,20 +26,12 @@ menuState.prototype = {
         this.loadJSONs();
     },
 
+    // TODO: Cargar JSON en preloadState
     loadJSONs: function () {
         for (let level = 0; level < 3; ++level) {
             let key = JSON_KEY + level;
             let jsonFile = LEVELS_JSON_DIR + LEVEL_JSON_PREFIX + level + LEVEL_JSON_SUFFIX;
-
-            console.dir(key);
-            console.dir(jsonFile);  
-
             game.load.json(key, jsonFile, true);
-            let levelData = game.cache.getJSON(key);
-            // let parsedData = JSON.parse(JSON.stringify(levelData));
-
-            console.dir(levelData);
-            // console.dir(parsedData);
         }
     },
 
@@ -59,6 +51,31 @@ menuState.prototype = {
 
     displayLevelCards: function () {
         this.levelCardsLayer = game.add.group();
+
+        for (let level = 0; level < 3; ++level) {
+            let key = JSON_KEY + level;
+            let levelData = game.cache.getJSON(key);
+
+            let cardSpriteKey = this.getSpriteKey(levelData.difficulty);
+
+            let x = LEVEL_CARDS_X;
+            let y = FIRST_LEVEL_Y + level * LEVEL_CARDS_Y_OFFSET;
+            let levelCard = game.add.sprite(x, y, cardSpriteKey);
+            levelCard.anchor.set(0.5, 0.5);
+        }
+    },
+
+    getSpriteKey: function (difficulty) {
+        switch (difficulty) {
+            case 0:
+                return EASY_LEVEL_CARD_SPRITE;
+            case 1:
+                return NORMAL_LEVEL_CARD_SPRITE;
+            case 2:
+                return HARD_LEVEL_CARD_SPRITE;
+            default:
+                console.error("Unknow difficulty value \"" + difficulty + "\" found in level JSON");
+        }
     },
 
     displayPlayerScores: function () {
