1fcf773b0a42792d74325402cb4335401186fcd0
Aitor Iribar <aitor.iribar@gmail.com>
2019-10-16 11:59:53 +0200

    Se carga el nivel medio

--- src/States/preloadState.js
+++ src/States/preloadState.js
@@ -86,7 +86,7 @@ preloadState.prototype = {
 
     update: function() {
         if (game.input.activePointer.isDown) {
-            game.state.start("levelLoadState", true, false, "resources/levels/lvl_01.json");   
+            game.state.start("levelLoadState", true, false, "resources/levels/lvl_02.json");   
         }
     },
     
