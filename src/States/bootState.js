50abe87c87cd53ac52e6d61af92182c3d8293e68
César Romero Albertos <43405217+CesarRomeroAlbertos@users.noreply.github.com>
2019-10-18 12:35:35 +0200

    Ajuste a prueba sonido

--- src/States/bootState.js
+++ src/States/bootState.js
@@ -9,9 +9,11 @@ bootState.prototype = {
         game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
         game.scale.setResizeCallback(this.onResize, this);
         game.physics.startSystem(Phaser.Physics.ARCADE);
+        
 
+        
         if (this.game.device.android && this.game.device.chrome && this.game.device.chromeVersion >= 55) {
-            this.game.sound.setTouchLock();
+            this.game.sound.touchLocked = true;
             this.game.input.touch.addTouchLockCallback(function () {
                 if (this.noAudio || !this.touchLocked || this._unlockSource !== null) {
                     return true;
@@ -44,6 +46,7 @@ bootState.prototype = {
             }, this.game.sound, true);
         }
 
+        
         //game.sound.mute = MUTE_AUDIO;
     },
 
